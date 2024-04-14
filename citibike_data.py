4d05d24ca5d547dd5c4842f79553831e8d8d2c87
BREONAEATS <breonaeats@gmail.com>
2024-04-14 16:11:29 -0700

    update

---
+++ citibike_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Number of Months
+n = 12
+citibike_full_data = pd.DataFrame()
+
+# For Loop to read each month's CSV and add month's data to full dataframe
+
+for x in range(1, n+1):
+    
+    if x < 10:
+        month = f'0{x}'
+    else:
+        month = x
+    
+    df = pd.read_csv(f'citibike-data/2022/JC-2022{month}-citibike-tripdata.csv')
+    
+    citibike_full_data = pd.concat([citibike_full_data, df])
+
+citibike_full_data
